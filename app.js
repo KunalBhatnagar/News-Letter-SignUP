5926fa5daf404a1ee9d0603df8978be8472c1ba9
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2020-10-07 20:15:31 +0530

    change page

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.post("/failure",function(req,res){
 });
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("server is running");
 });
